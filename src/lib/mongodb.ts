52e6419932c273696e8d0552ac6cf6bfdd50c10c
Saad Bin Ather <saadbinatherr@gmail.com>
2025-07-23 15:08:58 +0500

    Added ENV

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI =
-  process.env.MONGODB_URI ||
   "mongodb+srv://saadbinather:saad123@cluster0.gpehyiv.mongodb.net/";
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
